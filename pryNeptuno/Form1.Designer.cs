f2cd4bbb277842872d200f84573645945f2513cf
root <mgiraudo@ies21.edu.ar>
2023-05-17 13:29:08 -0300

    Agregar archivos de proyecto.

---
+++ pryNeptuno/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace pryNeptuno
+{
+    partial class frmNeptuno
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnMostrarTodos = new System.Windows.Forms.Button();
+            this.mrcFiltrar = new System.Windows.Forms.GroupBox();
+            this.cmbCargos = new System.Windows.Forms.ComboBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblCargos = new System.Windows.Forms.Label();
+            this.lblCiudad = new System.Windows.Forms.Label();
+            this.cmbMostrarCiudad = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            this.mrcFiltrar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dgvClientes.Location = new System.Drawing.Point(40, 111);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.Size = new System.Drawing.Size(744, 150);
+            this.dgvClientes.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Numero de Compañia";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre del Contacto";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cargo del Contacto";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Direccion";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Codigo postal";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Pais";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Fax";
+            this.Column7.Name = "Column7";
+            // 
+            // btnMostrarTodos
+            // 
+            this.btnMostrarTodos.Location = new System.Drawing.Point(525, 28);
+            this.btnMostrarTodos.Name = "btnMostrarTodos";
+            this.btnMostrarTodos.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrarTodos.TabIndex = 2;
+            this.btnMostrarTodos.Text = "Todos";
+            this.btnMostrarTodos.UseVisualStyleBackColor = true;
+            this.btnMostrarTodos.Click += new System.EventHandler(this.btnMostrarTodos_Click);
+            // 
+            // mrcFiltrar
+            // 
+            this.mrcFiltrar.Controls.Add(this.lblCiudad);
+            this.mrcFiltrar.Controls.Add(this.cmbMostrarCiudad);
+            this.mrcFiltrar.Controls.Add(this.lblCargos);
+            this.mrcFiltrar.Controls.Add(this.cmbCargos);
+            this.mrcFiltrar.Controls.Add(this.btnMostrarTodos);
+            this.mrcFiltrar.Location = new System.Drawing.Point(40, 23);
+            this.mrcFiltrar.Name = "mrcFiltrar";
+            this.mrcFiltrar.Size = new System.Drawing.Size(617, 71);
+            this.mrcFiltrar.TabIndex = 5;
+            this.mrcFiltrar.TabStop = false;
+            this.mrcFiltrar.Text = "Filtrar por";
+            // 
+            // cmbCargos
+            // 
+            this.cmbCargos.FormattingEnabled = true;
+            this.cmbCargos.Location = new System.Drawing.Point(60, 30);
+            this.cmbCargos.Name = "cmbCargos";
+            this.cmbCargos.Size = new System.Drawing.Size(91, 21);
+            this.cmbCargos.TabIndex = 3;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(709, 51);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar Tabla";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblCargos
+            // 
+            this.lblCargos.AutoSize = true;
+            this.lblCargos.Location = new System.Drawing.Point(14, 35);
+            this.lblCargos.Name = "lblCargos";
+            this.lblCargos.Size = new System.Drawing.Size(40, 13);
+            this.lblCargos.TabIndex = 4;
+            this.lblCargos.Text = "Cargos";
+            // 
+            // lblCiudad
+            // 
+            this.lblCiudad.AutoSize = true;
+            this.lblCiudad.Location = new System.Drawing.Point(167, 35);
+            this.lblCiudad.Name = "lblCiudad";
+            this.lblCiudad.Size = new System.Drawing.Size(40, 13);
+            this.lblCiudad.TabIndex = 6;
+            this.lblCiudad.Text = "Ciudad";
+            // 
+            // cmbMostrarCiudad
+            // 
+            this.cmbMostrarCiudad.FormattingEnabled = true;
+            this.cmbMostrarCiudad.Location = new System.Drawing.Point(213, 30);
+            this.cmbMostrarCiudad.Name = "cmbMostrarCiudad";
+            this.cmbMostrarCiudad.Size = new System.Drawing.Size(91, 21);
+            this.cmbMostrarCiudad.TabIndex = 5;
+            // 
+            // frmNeptuno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 292);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.mrcFiltrar);
+            this.Controls.Add(this.dgvClientes);
+            this.Name = "frmNeptuno";
+            this.Text = "Neptuno";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            this.mrcFiltrar.ResumeLayout(false);
+            this.mrcFiltrar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dgvClientes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.Button btnMostrarTodos;
+        private System.Windows.Forms.GroupBox mrcFiltrar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.ComboBox cmbCargos;
+        private System.Windows.Forms.Label lblCiudad;
+        private System.Windows.Forms.ComboBox cmbMostrarCiudad;
+        private System.Windows.Forms.Label lblCargos;
+    }
+}
+
---
+++ pryNeptuno/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace pryNeptuno
+{
+    public partial class frmNeptuno : Form
+    {
+        OleDbConnection conectarDB;
+        OleDbCommand comandoDB;
+        OleDbDataReader dataReaderDB;
+
+
+        public frmNeptuno()
+        {
+            InitializeComponent();
+        }
+        public void conectarBASE()
+        {
+            try
+            {
+                conectarDB = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:/NEPTUNO.accdb");
+                comandoDB = new OleDbCommand();
+                conectarDB.Open();
+
+                comandoDB.Connection = conectarDB;
+                comandoDB.CommandType = CommandType.TableDirect;
+                comandoDB.CommandText = "Clientes";
+
+                MessageBox.Show("Conexion a Tabla: " + comandoDB.CommandText);
+
+                dataReaderDB = comandoDB.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conectarBASE();
+            while (dataReaderDB.Read())
+            {
+                cmbCargos.Items.Add(dataReaderDB[3]);
+            }
+        }
+
+        private void btnMostrarTodos_Click(object sender, EventArgs e)
+        {
+            conectarBASE();
+            while (dataReaderDB.Read())
+            {
+                dgvClientes.Rows.Add(dataReaderDB[1], dataReaderDB[2], dataReaderDB[3], dataReaderDB[4], dataReaderDB[7], dataReaderDB[8], dataReaderDB[10]);
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            dgvClientes.Rows.Clear();
+        }
+    }
+}
